ad012f2bdd985a788acad097273607e8aea5a571
Ellad Tadmor <tadmor@aem.umn.edu>
2016-01-10 22:10:43 -0600

    Added description of UMD and TMS meetings

---
+++ emailaddr.js
@@ -0,0 +1,10 @@
+<!--
+
+function writemail(mailhost,login){
+  if (mailhost == "") {
+    mailhost = "umn.edu";
+  }
+  document.write("<a href=mailto:" + login + "@" + mailhost + ">" + login + "@" + mailhost + "</a>");
+}
+
+//-->
